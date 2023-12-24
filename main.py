4971f580db9f436c8b4c31ee0d8d9714926d4793
George <appeahg@outlook.com>
2023-12-24 19:06:36 +0000

    Updating the GUI with with tkinter

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import random
 import customtkinter
-#Creating a class for the GUI window with customertkinter
+import tkinter as tk
+#Creating a class for the GUI window with customertkinter and tkinter
 
 app = customtkinter.CTk()
-app.geometry("700x700")
+app.geometry("500x700")
+app.title("Dice Roller")
 
-Label = customtkinter.CTkLabel(app, text="", font=("Arial", 300))
+
+label_sub = customtkinter.CTkLabel(master=app, text="Press the button to roll the die:", font=('Arial', 20))
+label_sub.place(relx=0.5, rely=0.12, anchor=tk.CENTER)
+
+Label = customtkinter.CTkLabel(app, text="", font=("Arial", 150))
 
 def roll():
     number = random.randint(1,6)
@@ -14,7 +20,7 @@ def roll():
 def button_callback():
     print("Die roll")
 
-button = customtkinter.CTkButton(app, text="Roll die...",fg_color=("#DB3E39", "#821D1A"),hover_color="green",height=50, width=100,anchor="center", command=roll)
+button = customtkinter.CTkButton(app, text="Roll Die...",font=('Arial', 13), text_color=("gray10", "#DCE4EE"),fg_color=("#DB3E39", "#821D1A"),hover_color="green",height=50, width=100,anchor="center", command=roll)
 button.pack(padx=20, pady=20)
 
 app.mainloop()
